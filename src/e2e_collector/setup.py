d133b68d6f684910a98223fd0987a832bece9c46
siddartha00 <allenisaisiddartha@gmail.com>
2026-06-19 10:24:16 -0400

    remove wall follower

--- src/e2e_collector/setup.py
+++ src/e2e_collector/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/collector.launch.py',
-                                               'launch/wall_follower.launch.py']),
+                                               ]),
         ('share/' + package_name + '/config', ['config/nav2_params.yaml']),
     ],
     install_requires=['setuptools'],
